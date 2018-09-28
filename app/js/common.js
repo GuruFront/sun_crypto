a11e3ddff1e11c5c2b0130d985563b9a3274bbaa
GuruFront <ruslan.gunder@gmail.com>
2018-09-28 20:31:13 +0300

    new layouts added

--- app/js/common.js
+++ app/js/common.js
@@ -8,4 +8,14 @@
             preloader: false,
             fixedContentPos: false
         });
+
+        $(".scroll").on("click", "a", function(i) {
+            i.preventDefault();
+            var e = $(this).attr("href"),
+                t = $(e).offset().top;
+            $("body,html").animate({
+                scrollTop: t
+            }, 1500);
+        });
+
     });
